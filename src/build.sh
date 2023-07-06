2c4ae232386b547a92ba41429ae1da7a2a5ae82a
Hostine-254 <hostineomondi2020@gmail.com>
2023-07-06 12:53:21 -0700

    added build.sh

---
+++ src/build.sh
@@ -0,0 +1,10 @@
+
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Statics"
+python3.9 manage.py collectstatic --no input --clear
