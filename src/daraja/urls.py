8cfe7ceb2c8ff6cf3966dd3e6a5d46496b1a19df
Hostine-254 <hostineomondi2020@gmail.com>
2023-07-06 14:00:33 -0700

    added additional config

--- src/daraja/urls.py
+++ src/daraja/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import index
 
@@ -24,3 +26,4 @@ urlpatterns = [
     path('',index),
     path('api-auth/', include('rest_framework.urls')),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
